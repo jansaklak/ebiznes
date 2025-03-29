6569dc3f3a0cde1c29c3d46ddc11eb7a5d44ec95
jansaklak <101001672+jansaklak@users.noreply.github.com>
2025-03-29 17:49:41 +0100

    Update Product.scala

--- Zadanie2/retro-shop/app/models/Product.scala
+++ Zadanie2/retro-shop/app/models/Product.scala
@@ -1,9 +1,3 @@
 package models
 
-import play.api.libs.json._
-
-case class Product(id: Int, name: String, description: String, price: Double)
-
-object Product {
-  implicit val productFormat: OFormat[Product] = Json.format[Product]
-}
+case class Product(id: Long, name: String, description: String, price: Double)
