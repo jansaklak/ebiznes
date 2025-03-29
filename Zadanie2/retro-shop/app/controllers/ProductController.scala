fd41aa30df7e2c6cfc6cc8e290cfbb11b4dceff2
jansaklak <101001672+jansaklak@users.noreply.github.com>
2025-03-29 17:51:19 +0100

    Update ProductController.scala

--- Zadanie2/retro-shop/app/controllers/ProductController.scala
+++ Zadanie2/retro-shop/app/controllers/ProductController.scala
@@ -1,60 +1,79 @@
 package controllers
 
-import javax.inject._
-import play.api.mvc._
-import play.api.libs.json._
 import models.Product
+import play.api.libs.json._
+import play.api.mvc._
+import javax.inject._
 
 @Singleton
-class ProductController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-
+class ProductsController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  
+  implicit val productWrites: Writes[Product] = Json.writes[Product]
+  implicit val productReads: Reads[Product] = Json.reads[Product]
+  
   private var products = List(
     Product(1, "Pentium II", "Procesor Intel Pentium II 350 MHz", 150.00),
     Product(2, "Voodoo 3", "Karta graficzna 3dfx Voodoo 3 3000", 300.00),
-    Product(3, "Sound Blaster 16", "Karta dźwiękowa Creative Sound Blaster 16", 80.00)
+    Product(3, "Sound Blaster 16", "Karta dźwiękowa Creative Sound Blaster 16", 80.00),
+    Product(4, "RAM 128MB", "Pamięć RAM SDRAM 128MB", 120.00),
+    Product(5, "Hard Drive 20GB", "Dysk twardy IDE 20GB 5400 RPM", 200.00),
+    Product(6, "CD-ROM 52x", "Napęd CD-ROM 52x", 70.00)
   )
-
-  def list(): Action[AnyContent] = Action {
-    Ok(Json.toJson(products))
-  }
-
-  def get(id: Int): Action[AnyContent] = Action {
-    products.find(_.id == id) match {
-      case Some(product) => Ok(Json.toJson(product))
-      case None => NotFound(Json.obj("error" -> "Product not found"))
-    }
-  }
-
-  def create(): Action[JsValue] = Action(parse.json) { request =>
-    request.body.validate[Product] match {
-      case JsSuccess(newProduct, _) =>
+  
+  private var idCounter: Long = products.map(_.id).max + 1
+  
+  def create() = Action(parse.json) { request =>
+    request.body.validate[Product].fold(
+      errors => {
+        BadRequest(Json.obj("message" -> JsError.toJson(errors)))
+      },
+      product => {
+        val newProduct = product.copy(id = idCounter)
+        idCounter += 1
         products = products :+ newProduct
         Created(Json.toJson(newProduct))
-      case JsError(errors) =>
-        BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errors.toString))
-    }
+      }
+    )
   }
-
-  def update(id: Int): Action[JsValue] = Action(parse.json) { request =>
-    request.body.validate[Product] match {
-      case JsSuccess(updatedProduct, _) =>
-        products.find(_.id == id) match {
-          case Some(_) =>
-            products = products.map(p => if (p.id == id) updatedProduct else p)
-            Ok(Json.toJson(updatedProduct))
-          case None => NotFound(Json.obj("error" -> "Product not found"))
-        }
-      case JsError(errors) =>
-        BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errors.toString))
-    }
+  
+  def list() = Action {
+    Ok(Json.toJson(products))
   }
-
-  def delete(id: Int): Action[AnyContent] = Action {
+  
+  def get(id: Long) = Action {
     products.find(_.id == id) match {
-      case Some(_) =>
+      case Some(product) => Ok(Json.toJson(product))
+      case None => NotFound(Json.obj("message" -> s"Product with id $id not found"))
+    }
+  }
+  
+  def update(id: Long) = Action(parse.json) { request =>
+    request.body.validate[Product].fold(
+      errors => {
+        BadRequest(Json.obj("message" -> JsError.toJson(errors)))
+      },
+      product => {
+        products.find(_.id == id) match {
+          case Some(_) => 
+            // Remove old product and add updated one
+            products = products.filterNot(_.id == id)
+            val updatedProduct = product.copy(id = id)
+            products = products :+ updatedProduct
+            Ok(Json.toJson(updatedProduct))
+          case None => 
+            NotFound(Json.obj("message" -> s"Product with id $id not found"))
+        }
+      }
+    )
+  }
+  
+  def delete(id: Long) = Action {
+    products.find(_.id == id) match {
+      case Some(_) => 
         products = products.filterNot(_.id == id)
         NoContent
-      case None => NotFound(Json.obj("error" -> "Product not found"))
+      case None => 
+        NotFound(Json.obj("message" -> s"Product with id $id not found"))
     }
   }
 }
